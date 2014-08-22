ce596252e5461968015f0fc300981cbdebd6a8ef
gelahcem <gelahcem@gmail.com>
2014-08-22 11:23:36 +0200

    modelo base

--- TicketAlarm/src/com/example/ticketalarm/MainActivity.java
+++ TicketAlarm/src/com/example/ticketalarm/MainActivity.java
@@ -1,64 +1,47 @@
 package com.example.ticketalarm;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.EditText;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+   private String url1 = "http://api.openweathermap.org/data/2.5/weather?q=";
+   private String url2 = "&mode=xml";
+   private EditText location,country,temperature,humidity,pressure;
+   private HandleXML obj;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      location = (EditText)findViewById(R.id.editText1);
+      country = (EditText)findViewById(R.id.editText2);
+      temperature = (EditText)findViewById(R.id.editText3);
+      humidity = (EditText)findViewById(R.id.editText4);
+      pressure = (EditText)findViewById(R.id.editText5);
+   }
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+   // Inflate the menu; this adds items to the action bar if it is present.
+   getMenuInflater().inflate(R.menu.main, menu);
+   return true;
+   }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+   public void open(View view){
+      String url = location.getText().toString();
+      String finalUrl = url1 + url + url2;
+      country.setText(finalUrl);
+      obj = new HandleXML(finalUrl);
+      obj.fetchXML();
+      while(obj.parsingComplete);
+      country.setText(obj.getCountry());
+      temperature.setText(obj.getTemperature());
+      humidity.setText(obj.getHumidity());
+      pressure.setText(obj.getPressure());
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
+   }
 
 }
